f2f096b85c00aaffe3096e07e8ff167e1e74a85c
nabyungho <valoon2@naver.com>
2025-08-07 18:08:53 +0900

    [feat] init

---
+++ src/test/java/com/app/wiseaiassignment/WiseaiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wiseaiassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseaiAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
