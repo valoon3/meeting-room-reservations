404b26e96a7fc7d34daacd47d3831143eb3d1e9c
nabyungho <valoon2@naver.com>
2025-08-11 15:27:09 +0900

    [feat] BPayGateway, CPayGateway 생성

---
+++ src/main/java/com/app/meetingRoomReservation/api/payment/gateway/CPayGateway.java
@@ -0,0 +1,31 @@
+package com.app.meetingRoomReservation.api.payment.gateway;
+
+import com.app.meetingRoomReservation.api.payment.constant.ProviderType;
+import com.app.meetingRoomReservation.api.payment.dto.MockPaymentRequest;
+import com.app.meetingRoomReservation.api.payment.entity.Payment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
+public class CPayGateway implements PaymentGateway {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${payment-mock-server.c-pay-url}")
+    private String apiUrl;
+
+    @Override
+    public void requestPayment(Payment payment) {
+        MockPaymentRequest request = new MockPaymentRequest(payment.getId(), payment.getTotalPrice());
+        restTemplate.postForEntity(apiUrl, request, Void.class);
+    }
+
+    @Override
+    public ProviderType getProviderType() {
+        return ProviderType.C_PAY;
+    }
+
+}
